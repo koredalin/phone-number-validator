a0be81875f37caad563f04234d33fc9b09bb7829
koredalin <koredalin@yahoo.com>
2021-07-18 09:49:28 +0300

    Doctrine migrations removed as no time to comply

--- migrations-db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once './vendor/autoload.php';
-require_once './config/db_config.php';
-
-use Doctrine\DBAL\DriverManager;
-
-return DriverManager::getConnection(DB_CONNECTION_CONFIGURATION);
