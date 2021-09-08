932e90a0e85df8da29ea8d251ec34a8f082b431f
koredalin <koredalin@yahoo.com>
2021-09-08 14:35:20 +0300

    Routes  refactoring

--- src/Routes.php
+++ src/Routes.php
@@ -1,13 +1,16 @@
 <?php
-//declare(strict_types=1);
 
 namespace App;
 
+// Libraries
 use DI\Container;
 use League\Route\RouteCollectionInterface;
 use League\Route\Router;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+// Routes
+use App\Controllers\RouteConstants as RC;
+// Controllers
 use App\Http\Controllers\GreetingsController;
 use App\Controllers\UserController;
 use App\Controllers\ConfirmationController;
@@ -23,27 +26,26 @@ class Routes
 {
     public static function routes(Container $container, Router $router): RouteCollectionInterface
     {
-        
         $router->map('GET', URL_SUBFOLDER.'/', function (ServerRequestInterface $request) use ($container): ResponseInterface {
             $response = $container->get('response');
-            $response->getBody()->write('<h2>Maskaaaaa</h2>');
+            $response->getBody()->write('<h2>You are welcome :-)</h2>');
             
             return $response;
         });
-        
         $router->map('GET', URL_SUBFOLDER.'/hello/{name}', GreetingsController::class.'::index');
         $router->map('GET', URL_SUBFOLDER.'/add/{name}', GreetingsController::class.'::store');
         $router->map('GET', URL_SUBFOLDER.'/user', UserController::class.'::index');
         
-        $router->map('POST', URL_SUBFOLDER.'/registration/phone-code-number', RegistrationController::class.'::registrateFormFromPhoneCodeNumber');
-        $router->map('POST', URL_SUBFOLDER.'/registration/assembled-phone-number', RegistrationController::class.'::registrateFormFromAssembledPhoneNumber');
-            
-        $router->map('POST', URL_SUBFOLDER.'/confirmation/{transactionId}', ConfirmationController::class.'::index');
-        $router->map('GET', URL_SUBFOLDER.'/reset-code/{transactionId}', ConfirmationController::class.'::resetCode');
-        
-        $router->map('GET', URL_SUBFOLDER.'/transaction-info/{transactionId}', TransactionInfoController::class.'::index');
-        
-//        print_r($route); exit;
+        /**********************************************************************/
+        /******************************* API **********************************/
+        // Registration
+        $router->map('POST', URL_SUBFOLDER.RC::REGISTRATION.'/phone-code-number', RegistrationController::class.'::registrateFormFromPhoneCodeNumber');
+        $router->map('POST', URL_SUBFOLDER.RC::REGISTRATION.'/assembled-phone-number', RegistrationController::class.'::registrateFormFromAssembledPhoneNumber');
+        // Confirmation
+        $router->map('POST', URL_SUBFOLDER.RC::CONFIRMATION.'/{transactionId}', ConfirmationController::class.'::index');
+        $router->map('GET', URL_SUBFOLDER.RC::RESET_CODE.'/{transactionId}', ConfirmationController::class.'::resetCode');
+        // Info
+        $router->map('GET', URL_SUBFOLDER.RC::TRANSACTION_INFO.'/{transactionId}', TransactionInfoController::class.'::index');
         
         return $router;
     }
