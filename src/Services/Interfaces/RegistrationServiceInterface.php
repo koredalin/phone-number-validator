7accec3b9576778b4838d117d5268a6a05287fc2
koredalin <koredalin@yahoo.com>
2021-09-25 23:34:26 +0300

    Not used code deleted

--- src/Services/Interfaces/RegistrationServiceInterface.php
+++ src/Services/Interfaces/RegistrationServiceInterface.php
@@ -4,7 +4,6 @@ namespace App\Services\Interfaces;
 
 use App\Services\Interfaces\WebPageServiceInterface;
 // Input
-use App\Controllers\Input\Models\RegistrationModel;
 use App\Controllers\Input\Models\RegistrationModelPhoneCodeNumber;
 use App\Controllers\Input\Models\RegistrationModelAssembledPhoneNumber;
 // Entities
@@ -16,16 +15,6 @@ use App\Entities\PhoneConfirmation;
  */
 interface RegistrationServiceInterface extends WebPageServiceInterface
 {
-//    public function createFormFromPhoneCodeNumber(string $requestBody): RegistrationModelPhoneCodeNumber;
-    
-//    public function createFormFromAssembledPhoneNumber(string $requestBody): RegistrationModelAssembledPhoneNumber;
-    
-//    public function isValidForm(): bool;
-    
-//    public function getFormErrors(): string;
-    
-//    public function registrate(RegistrationModel $form): PhoneConfirmation;
-    
     public function registratePhoneCodeNumber(RegistrationModelPhoneCodeNumber $form): PhoneConfirmation;
     
     public function registrateAssembledPhoneNumber(RegistrationModelAssembledPhoneNumber $form): PhoneConfirmation;
